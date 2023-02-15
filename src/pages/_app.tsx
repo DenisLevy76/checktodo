efd0511af063ed59093cef4e45178ec922d96e75
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-02-15 20:29:25 -0300

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Roboto } from '@next/font/google'
+import { NextUIProvider } from '@nextui-org/react'
+import { darkTheme } from '@/styles/darkTheme'
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={roboto.className}>
+      <NextUIProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </div>
+  )
 }
